25ccbe0d6ceec6ad37974d653893b98ccbecb42a
kiruthiyaashree <kiruthiyaashreesp@gmail.com>
2024-01-30 19:06:28 +0530

    changes

--- carrental/src/App.jsx
+++ carrental/src/App.jsx
@@ -3,11 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Landing from './components/Landing'
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import SignIn from './components/SignIn'
+import SignUp from './components/SignUp'
 function App() {
   return (
     <>
-      <Landing/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Landing/>}/>
+          <Route path='/signin' element={<SignIn/>}/>
+          <Route path='/signup' element={<SignUp/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
--- carrental/src/components/SignUp.jsx
+++ carrental/src/components/SignUp.jsx
@@ -2,7 +2,23 @@ const SignUp=()=>
 {
     return (
         <>
-        <p>Signin</p>
+            <div className="">
+                <form className="flex justify-center items-center min-h-[100vh]">
+                    <fieldset className="border border-black w-fit p-32 rounded-xl">
+                    <input type="text" className="border border-black p-3 rounded-md my-3" placeholder="username" id="username" name="username"/>
+                    <br/>
+                    <input type="text" className="border border-black p-3 rounded-md my-3" placeholder="email"id="email" name="email" />
+                    <br/>
+                    <input type="password" className="border border-black p-3 rounded-md my-3" placeholder="password" id="password" name="password"/>
+                    <br/>
+                    <input type="password" className="border border-black p-3 rounded-md my-3" placeholder="confirm_passsword" id="confirm_password" name="comfirm_password"/>
+                    <br/>
+                    <div className="flex justify-center my-3">
+                    <button className="px-8 py-2 border border-black rounded-xl bg-black text-white">Signup</button>
+                    </div>
+                    </fieldset>
+                </form>
+            </div>
         </>
     )
 }
