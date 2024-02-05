8d305b42fc0d5ea8b954cead16a535b705094a91
kiruthiyaashree <kiruthiyaashreesp@gmail.com>
2024-02-05 19:03:59 +0530

    account details css added

--- carrental/src/components/Profile.jsx
+++ carrental/src/components/Profile.jsx
@@ -1,8 +1,22 @@
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import { useNavigate } from "react-router-dom";
+
 const Profile =()=>
 {
+    const navigate=useNavigate();
+    const handleBack=()=>
+    {
+        navigate("/");
+    }
     return (
         <>
-            <p>Profile</p>
+            <div className="m-12">
+                <div className='flex justify-between items-center'>
+                    <button onClick={handleBack} className='flex p-2 hover:bg-gray-400 hover:rounded-xl hover:p-2'><ArrowBackIcon/></button>
+                    <p className="font-bold flex items-center"><PersonOutlineIcon/>{localStorage.getItem('userName').replace(/"/g,'')}</p>
+                </div>
+            </div>
         </>
     )
 };
